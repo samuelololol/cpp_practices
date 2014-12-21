b939856c521392b2314dc87e93c6a8110296416d
samuel <samuelololol@gmail.com>
2014-12-21 16:30:31 +0800

    test concate

--- shared_ptr_with_string_and_char.cpp
+++ shared_ptr_with_string_and_char.cpp
@@ -12,8 +12,10 @@ using std::endl;
 std::tr1::shared_ptr<std::string> char_to_string(std::tr1::shared_ptr<std::string>);
 void reuse_stringptr(std::tr1::shared_ptr<std::string>);
 void string_to_char(std::tr1::shared_ptr<std::string>);
+void string_concate(std::tr1::shared_ptr<std::string>);
 
-int main(){
+int main()
+{
     std::tr1::shared_ptr<std::string> ptr;
     cout << "counts of ptr: " << ptr.use_count() << endl;
 
@@ -28,11 +30,18 @@ int main(){
     // string to char
     string_to_char(ptr);
     cout << "counts of ptr: " << ptr.use_count() << endl;
+
+    // string concate
+    string_concate(ptr);
+    cout << "counts of ptr: " << ptr.use_count() << endl;
+    cout << "*ptr show in main: " << *ptr << endl;
+
     return 0;
 }
 
 
-std::tr1::shared_ptr<std::string> char_to_string(std::tr1::shared_ptr<std::string> strptr){
+std::tr1::shared_ptr<std::string> char_to_string(std::tr1::shared_ptr<std::string> strptr)
+{
     cout << "================================================================================" << endl;
     strptr = std::tr1::shared_ptr<std::string>(new std::string("hahahaha"));
     cout << "address of strptr: " << strptr << ", strptr: " << *strptr << endl;
@@ -42,13 +51,17 @@ std::tr1::shared_ptr<std::string> char_to_string(std::tr1::shared_ptr<std::strin
     return strptr;
 }
 
-void reuse_stringptr(std::tr1::shared_ptr<std::string> reuseptr){
+
+void reuse_stringptr(std::tr1::shared_ptr<std::string> reuseptr1)
+{
     cout << "================================================================================" << endl;
-    cout << "address of reuseptr: " << reuseptr << ", reuseptr: " << *reuseptr << endl;
-    cout << "counts of reuseptr: " << reuseptr.use_count() << endl;
+    cout << "address of reuseptr1: " << reuseptr1 << ", reuseptr1: " << *reuseptr1 << endl;
+    cout << "counts of reuseptr1: " << reuseptr1.use_count() << endl;
 }
 
-void string_to_char(std::tr1::shared_ptr<std::string> reuseptr2){
+
+void string_to_char(std::tr1::shared_ptr<std::string> reuseptr2)
+{
     cout << "================================================================================" << endl;
     char* c = (char*)malloc(reuseptr2->size()*sizeof(char));
     strcpy(c, reuseptr2->c_str());
@@ -58,3 +71,14 @@ void string_to_char(std::tr1::shared_ptr<std::string> reuseptr2){
     cout << "address of reuseptr2: " << reuseptr2 << ", reuseptr2: " << *reuseptr2 << endl;
     cout << "address of reuseptr2.get(): " << reuseptr2.get() << endl;
 }
+
+
+void string_concate(std::tr1::shared_ptr<std::string> reuseptr3)
+{
+    cout << "================================================================================" << endl;
+    std::string to_concate_string("_postfix");
+    cout << "address of to_concate_string: " << &to_concate_string << ", to_concate_string: " << to_concate_string << endl;
+    *reuseptr3 = *reuseptr3 + to_concate_string;
+    cout << "address of reuseptr3: " << reuseptr3 << ", reuseptr3: " << *reuseptr3 << endl;
+}
+
